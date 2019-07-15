cf2d9459a56ccf368e06249c27b71c60647d80e7
lluuii <u0859753@gmail.com>
2019-07-15 21:31:41 +0300

    complite'

--- prettify.js
+++ prettify.js
@@ -3,6 +3,21 @@
 //   text
 //   <div>Bam</div>
 // </body>
+
+function prettify(doc) {
+	const nodelist = doc.body.childNodes
+
+	for(let item of nodelist) {
+		if (item.nodeType === 1 && item.nodeName.toLowerCase() === 'div') {
+			const text = item.innerText
+			item.innerText = ''
+			const p = item.appendChild(document.createElement("p"))
+			p.innerHTML = text
+		} else continue
+	}
+}
+
+
 const elements = prettify(document);
 console.log(document.body.innerHTML);
 // <body>
--- recSearchTags.js
+++ recSearchTags.js
@@ -3,6 +3,43 @@
 //   text
 //   <div><p>2</p></div>
 // </body>
+
+
+//first version
+function recSearchTags(document, tagName) {
+  return document.querySelectorAll(tagName);
+}
+
+// second version
+function recSearchTags(parent, tagName) {
+  const arrTags = [];
+
+  (function findTags(elem) {
+    let _parent;
+    let nodes;
+    if (elem === undefined && parent === document ) {
+      _parent = parent.body;
+      nodes = _parent.childNodes;
+    } else {
+      nodes = elem.childNodes;
+    }
+
+    for (let item of nodes) {
+      if (item.nodeType === 1  ) {
+
+    		if (item.childNodes.length !== 0 && item.nodeName.toLowerCase() === tagName)
+          arrTags.push(item)
+    		else findTags(item)
+
+      } else continue;
+    }
+  })();
+
+  return arrTags
+}
+
+
+
 const elements = recSearchTags(document, 'p');
 // ['<p>1</p>' '<p>2</p>'] где каждый элемент это объект соответствующего типа
 console.log(elements.length); // 2
