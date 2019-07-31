a2a9fbf5a6a5a728433242f55eb699d7d3bf662a
lluuii <u0859753@gmail.com>
2019-07-31 19:25:43 +0300

    Create gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const gulp      = require('gulp'),
+      htmlmin   = require('gulp-htmlmin'),
+      sass      = require('gulp-sass'),
+      cleanCSS  = require('gulp-clean-css');
+
+
+function minify_html(cb) {
+  gulp.src('src/**/*.html')
+    .pipe(htmlmin())
+    .pipe(gulp.dest('dist/'));
+  cb();
+}
+gulp.task(minify_html);
+
+function sassToCss(cb) {
+  gulp.src('src/**/*.scss')
+    .pipe(sass())
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dist/css'));
+  cb();
+}
+gulp.task(sassToCss);
+
+// gulp.watch(['/*.html', 'src/**/*.scss'], function(cb) {
+//   gulp.parallel(css, javascript)
+//   cb();
+// });
+
+function watchFiles() {
+  gulp.watch('src/**/*.html', minify_html);
+  gulp.watch('src/**/*.scss', sassToCss);
+}
+
+exports.default = gulp.series( gulp.parallel(minify_html, sassToCss), watchFiles);
